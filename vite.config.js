9fc1806077ad5e3020537c6bce33979aab407346
Shubham <shubhamchakraborty702@gmail.com>
2025-12-29 18:16:59 +0530

    Final Changes are relflected

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Shubhamzio/audionexz-cicd/',
+  base:"/audionexz-cicd",
   server: {
     port: 3000,
-
   },
 })
