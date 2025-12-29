888bbc1480532bd8bcef82b21421ae91798758cf
Shubham <shubhamchakraborty702@gmail.com>
2025-12-29 18:08:23 +0530

    commits are successful

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/https://github.com/Shubhamzio/audionexz-cicd/',
+  base:'/Shubhamzio/audionexz-cicd/',
   server: {
     port: 3000,
 
